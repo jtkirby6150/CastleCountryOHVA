f5a6a7274e9f81a7ecc3de8e2bcd3ac208a3b624
jtkirby6150 <james.t.kirby@gmail.com>
2021-01-03 14:54:45 -0700

    Update init.php

--- mainincludes/init.php
+++ mainincludes/init.php
@@ -3,7 +3,7 @@ ob_start();
 session_start();
 date_default_timezone_set('America/Denver');
 include "/home/ix4wmjzt9fz0/public_html/resources/functions.php";
-$conn = mysqli_connect('localhost', 'ccohva', 'secret!@#', 'ccohva');
+$conn = mysqli_connect('localhost', '', '', '');
 
 
 $logodir = "assets/img";
