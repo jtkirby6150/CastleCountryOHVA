7ffe9ecfdf4aafe47748ba00be10b6012e8a047b
Kirby <james.t.kirby@gmail.com>
2021-01-04 20:08:15 -0700

    starting fresh

--- public_html/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../mainincludes/init.php";
-session_start();
-session_unset();
-session_destroy();
-set_message("You have successfully logged out.", "success");
-redirect("../index.php");
-exit();
-?>
--- resources/functions.php
+++ resources/functions.php
@@ -142,6 +142,12 @@ function displayAdminNewsImages($img)
 }
 
 
+function clean($string)
+{
+    return htmlentities($string);
+}
+
+
 
 
 
