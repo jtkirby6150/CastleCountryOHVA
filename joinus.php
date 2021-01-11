9334867eb291ff51708830ab56f8ef110c348dea
Kirby <james.t.kirby@gmail.com>
2021-01-11 14:58:09 -0700

    troubleshooting

--- joinus.php
+++ joinus.php
@@ -3,7 +3,7 @@ include "mainincludes/header.php";
 include "mainincludes/init.php";
 include "mainincludes/nav.php";
 
-if(isset($_POST['submitRegistration'])){
+if(isset($_SERVER['submitRegistration'])){
     $member = escape($_POST['member']);
     $newsletter1 = escape($_POST['newsletter1']);
     $newsletter2 = escape($_POST['newsletter2']);
@@ -70,7 +70,7 @@ if(isset($_POST['submitRegistration'])){
         <?php display_message(); ?>
         <p style="text-align: center;" class='mb-5'>Please join us by filling out the this downloadable <a href="assets/2021-Membership-Application.pdf" download="">FORM</a> and sending it in.</p>
         <section id="mainForm" style="border: solid">
-            <form method="post" action="makepdf.php" class="p-4" enctype="multipart/form-data">
+            <form method="post" action="" class="p-4" enctype="multipart/form-data">
                 <div class="form-row">
                     <div class="col-md-4">
                         <div class="form-group">
