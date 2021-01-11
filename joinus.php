b7c40a10020b9e2cb1f95dd5df523a56ec6456d3
Kirby <james.t.kirby@gmail.com>
2021-01-11 15:09:29 -0700

    troubleshooting

--- joinus.php
+++ joinus.php
@@ -59,9 +59,9 @@ if(isset($_SERVER['submitRegistration'])){
         $helpList = rtrim($helpList, ", ");
     }
 
-    $addMember = query("INSERT INTO members (member, newsletter1, newsletter2, member1FN, member1LN, member2FN, member2LN, kid1, kid2, kid3, kid4, kid5, phone, email1, email2, address, pobox, city, state, zip, help, accept, signature, date, active) VALUES ('$member', '$newsletter1', '$newsletter2', '$member1FN', '$member1LN', '$member2FN', '$member2LN', '$kid1', '$kid2', '$kid3', '$kid4', '$kid5', '$phone', '$email1', '$email2', '$address', '$pobox', '$city', '$state', '$zip', '$helpList', '$accept', '$signature', '$date', 0)");
+    $addMember = query("INSERT INTO members (member, newsletter1, newsletter2, member1FN, member1LN, member2FN, member2LN, kid1, kid2, kid3, kid4, kid5, phone, email1, email2, address, pobox, city, state, zip, help, accept, signature, date, active) VALUES ('$member', '$newsletter1', '$newsletter2', '$member1FN', '$member1LN', '$member2FN', '$member2LN', '$kid1', '$kid2', '$kid3', '$kid4', '$kid5', '$phone', '$email1', '$email2', '$address', '$pobox', '$city', '$state', '$zip', '$helpList', '$accept', '$signature', '$date', 'Inactive')");
     confirm($addMember);
-    set_message("You have successfully registered. Please keep an eye out for an email from us with further instructions.");
+    set_message("You have successfully registered. Please keep an eye out for an email from us with further instructions.", "success");
     redirect("joinus.php");
     exit();
 }
