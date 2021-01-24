d0f183c4e4c60a91eb77acb113dfd10b3923b138
Kirby <james.t.kirby@gmail.com>
2021-01-24 09:46:56 -0700

    troubleshooting archive

--- admin/adminarchive.php
+++ admin/adminarchive.php
@@ -60,7 +60,7 @@ if(isset($_GET['updatearchive'])){
         $title = $archiveRow['title'];
         $date = $archiveRow['date'];
         $location = $archiveRow['location'];
-        $gps = htmlentities($archiveRow['gps']);
+        $gps = clean($archiveRow['gps']);
         $difficulty = $archiveRow['difficulty'];
         $duration = $archiveRow['duration'];
         $description = $archiveRow['description'];
